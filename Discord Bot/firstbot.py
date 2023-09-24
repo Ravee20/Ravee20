989ce95f78e27212e46f4a70f45fcf246b7f8ff2
Ravee20 <135257871+Ravee20@users.noreply.github.com>
2023-09-24 09:18:09 +0300

    Add files via upload

---
+++ Discord Bot/firstbot.py
@@ -0,0 +1,38 @@
+import discord
+import random
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# istemci (client) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("Bye!")
+    elif message.content.startswith("Şifre oluştur"):
+        sifre = gen_pass(10)
+        await message.channel.send(sifre)
+    else:
+        await message.channel.send(message.content)
+
+client.run("Discord Bot Token")
